efe1b7d2fcf4b6ce6fc21511081cdb5b822748e6
Wyrozumialski <spedrox.mail@gmail.com>
2021-05-13 20:26:08 +0200

    Added login button functionality

--- app/src/main/java/com/github/codeforgreen/itrash/LoginActivity.java
+++ app/src/main/java/com/github/codeforgreen/itrash/LoginActivity.java
@@ -1,8 +1,15 @@
 package com.github.codeforgreen.itrash;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.github.codeforgreen.itrash.tasks.LoginTask;
 
@@ -10,11 +17,30 @@ import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        TextView register = findViewById(R.id.login_register);
+        register.setMovementMethod(LinkMovementMethod.getInstance());
+    }
 
-        Button clickButton = (Button) findViewById(R.id.login_button);
-        clickButton.setOnClickListener(v -> new LoginTask(new JSONObject()).execute(null, null));
+    public void login_button(View view) {
+        EditText email = findViewById(R.id.login_email);
+        EditText password = findViewById(R.id.login_password);
+        //login functionality.
+
+        //Wersja robocza
+
+        if (email.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    public void register_button(View view){
+        Uri uri = Uri.parse("https://www.google.com");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 }
